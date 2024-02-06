7e088ebed9976b1c69a1c45676d772900fccf122
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2024-02-05 23:49:05 -0300

    Finish

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function tocaSom(idElementAudio) {
-    document.querySelector(idElementAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado.');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -8,7 +14,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
 for (let cont = 0; cont < listaDeTeclas.length; cont++) {
-    const tecla = listaDeTeclas[cont];    
+    const tecla = listaDeTeclas[cont];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
@@ -16,8 +22,11 @@ for (let cont = 0; cont < listaDeTeclas.length; cont++) {
         tocaSom(idAudio);
     };
 
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');        
+    tecla.onkeydown = function (evento) {
+        console.log(evento.code);
+        if (evento.code == 'Space' || evento.code == 'Enter' || evento.code == 'NumpadEnter') {
+            tecla.classList.add('ativa');
+        }
     }
     tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
